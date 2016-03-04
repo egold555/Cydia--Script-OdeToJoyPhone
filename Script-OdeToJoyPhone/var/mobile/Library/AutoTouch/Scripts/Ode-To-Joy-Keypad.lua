e98e39d1dac2ae1d0075471f3884930ed27bb234
egold555 <eric@golde.org>
2016-03-04 09:04:19 -0800

    Initial Release

---
+++ Script-OdeToJoyPhone/var/mobile/Library/AutoTouch/Scripts/Ode-To-Joy-Keypad.lua
@@ -0,0 +1,195 @@
+CREATETIME="2016-01-20 21:25:28";
+
+adaptResolution(750, 1334);
+adaptOrientation(ORIENTATION_TYPE.PORTRAIT);
+
+touchDown(7, 613, 366);
+usleep(233455.62);
+touchUp(7, 606, 372);
+usleep(200113.17);
+
+touchDown(7, 561, 382);
+usleep(233511.54);
+touchUp(7, 564, 392);
+usleep(233764.21);
+
+touchDown(8, 572, 575);
+usleep(66592.62);
+touchMove(8, 570, 584);
+usleep(16698.75);
+touchMove(8, 570, 587);
+usleep(16852.08);
+touchMove(8, 570, 589);
+usleep(16437.58);
+touchMove(8, 570, 590);
+usleep(99941.29);
+touchMove(8, 574, 590);
+usleep(16953.79);
+touchUp(8, 578, 594);
+usleep(233516.29);
+
+touchDown(3, 594, 747);
+usleep(316720.92);
+touchUp(3, 595, 752);
+usleep(166769.21);
+
+touchDown(3, 590, 719);
+usleep(250009.50);
+touchUp(3, 592, 719);
+usleep(200349.71);
+
+touchDown(8, 570, 561);
+usleep(233047.21);
+touchUp(8, 569, 559);
+usleep(200296.17);
+
+touchDown(7, 554, 400);
+usleep(233392.75);
+touchUp(7, 550, 401);
+usleep(183246.08);
+
+touchDown(9, 384, 366);
+usleep(250058.67);
+touchUp(9, 376, 370);
+usleep(166854.71);
+
+touchDown(1, 158, 381);
+usleep(216591.00);
+touchUp(1, 161, 378);
+usleep(202535.58);
+
+touchDown(1, 154, 391);
+usleep(197820.46);
+touchUp(1, 168, 397);
+usleep(316830.50);
+
+touchDown(9, 397, 395);
+usleep(233473.21);
+touchUp(9, 402, 395);
+usleep(250078.00);
+
+touchDown(7, 588, 393);
+usleep(216868.12);
+touchUp(7, 591, 397);
+usleep(216656.83);
+
+touchDown(8, 596, 574);
+usleep(517249.12);
+touchUp(8, 595, 571);
+usleep(233628.46);
+
+touchDown(9, 379, 366);
+usleep(116399.83);
+touchUp(9, 378, 377);
+usleep(149959.25);
+
+touchDown(9, 380, 391);
+usleep(567434.75);
+touchUp(9, 382, 393);
+usleep(450285.83);
+
+touchDown(7, 574, 404);
+usleep(216089.79);
+touchUp(7, 575, 397);
+usleep(266919.75);
+
+touchDown(7, 611, 391);
+usleep(216781.21);
+touchMove(7, 601, 401);
+usleep(16679.00);
+touchMove(7, 601, 402);
+usleep(16717.17);
+touchMove(7, 604, 405);
+usleep(16510.50);
+touchUp(7, 608, 409);
+usleep(316971.46);
+
+touchDown(8, 587, 560);
+usleep(233328.38);
+touchUp(8, 592, 562);
+usleep(383633.62);
+
+touchDown(3, 572, 756);
+usleep(183382.54);
+touchUp(3, 573, 758);
+usleep(283443.58);
+
+touchDown(3, 575, 764);
+usleep(250057.50);
+touchUp(3, 577, 766);
+usleep(300340.58);
+
+touchDown(8, 603, 581);
+usleep(233312.46);
+touchUp(8, 604, 580);
+usleep(316859.00);
+
+touchDown(7, 574, 396);
+usleep(249960.04);
+touchUp(7, 568, 404);
+usleep(383696.88);
+
+touchDown(9, 390, 390);
+usleep(266993.04);
+touchUp(9, 383, 390);
+usleep(484562.83);
+
+touchDown(1, 167, 391);
+usleep(215426.79);
+touchUp(1, 165, 386);
+usleep(250326.29);
+
+touchDown(1, 165, 388);
+usleep(200054.29);
+touchUp(1, 174, 395);
+usleep(383525.50);
+
+touchDown(9, 391, 402);
+usleep(216686.04);
+touchUp(9, 392, 405);
+usleep(1218104.50);
+
+touchDown(7, 581, 376);
+usleep(316390.75);
+touchUp(7, 584, 385);
+usleep(417067.25);
+
+touchDown(9, 369, 413);
+usleep(616955.75);
+touchMove(9, 371, 409);
+usleep(16608.71);
+touchMove(9, 371, 411);
+usleep(17081.96);
+touchUp(9, 371, 416);
+usleep(416432.29);
+
+touchDown(1, 181, 383);
+usleep(99582.67);
+touchUp(1, 182, 390);
+usleep(200314.96);
+
+touchDown(1, 182, 360);
+usleep(483974.21);
+touchMove(1, 179, 365);
+usleep(33291.75);
+touchMove(1, 179, 366);
+usleep(100211.08);
+touchMove(1, 179, 367);
+usleep(83218.58);
+touchMove(1, 179, 368);
+usleep(83452.58);
+touchMove(1, 179, 370);
+usleep(99976.79);
+touchMove(1, 179, 371);
+usleep(16724.92);
+touchMove(1, 179, 372);
+usleep(16616.79);
+touchMove(1, 179, 373);
+usleep(150010.17);
+touchMove(1, 179, 374);
+usleep(166758.62);
+touchMove(1, 179, 375);
+usleep(16640.42);
+touchMove(1, 179, 376);
+usleep(16608.46);
+touchUp(1, 182, 379);
